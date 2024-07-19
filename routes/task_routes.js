0b5e455c1df18c321c7c20564a5414ed16867854
niteshkumar257 <niteshreddy257@gmail.com>
2024-07-19 21:30:40 +0530

    update task_routes

--- routes/task_routes.js
+++ routes/task_routes.js
@@ -1,8 +1,18 @@
-import express from "express";
-const router=express.Router();
+import express from 'express';
+import {
+    createTask,
+    getTasksUnderProject,
+    getTaskById,
+    getAllTasksUnderProject
+} from '../controller/task_controller.js';
+import { validateCreateTask, validateGetTaskById } from '../utils/middleware/validator.js';
 
-router.get('/tasks/:user_id');
-router.post('takss/:user_id');
+const router = express.Router();
 
+
+router.post('/task/:project_id/add', validateCreateTask, createTask);
+router.get('/project/:project_id/user/:user_id/tasks', getTasksUnderProject);
+router.get('/tasks/:task_id', validateGetTaskById, getTaskById);
+router.get('/projects/:project_id/tasks', getAllTasksUnderProject);
 
 export default router;
