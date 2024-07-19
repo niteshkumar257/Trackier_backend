3a54fc271478ddab67d4e5d1c4820fee217cb611
niteshkumar257 <niteshreddy257@gmail.com>
2024-07-19 21:29:49 +0530

    bug fix user controller

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -5,17 +5,16 @@ import { client } from "../db/config.js";
 
 dotenv.config();
 
-const create_username=(email,name)=>{
-
-  return name.slice(0,4)+email.slice(0,5);
-
-  
-}
+const create_username = (email, name) => {
+  return name.slice(0, 4) + email.slice(0, 5);
+};
 const login = async (req, res) => {
   try {
-    const {email, password } = req.body;
+    const { email, password } = req.body;
 
-    const result = await client.query('SELECT * FROM users WHERE email = $1', [email]);
+    const result = await client.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
     const user = result.rows[0];
     console.log(user);
 
@@ -23,19 +22,14 @@ const login = async (req, res) => {
       return res.status(401).json({ error: "No User exists" });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.passwords);
-    console.log("asdnfk",isPasswordValid);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ error: "Incorrect Password" });
     }
 
-    const token = jwt.sign(
-      { userId: user.user_id },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "1h",
-      }
-    );
+    const token = jwt.sign({ userId: user.user_id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
 
     res.status(200).json({ token });
   } catch (error) {
@@ -46,8 +40,6 @@ const login = async (req, res) => {
 
 // Register User
 const register = async (req, res) => {
- 
-
   try {
     const { email, password, name } = req.body;
     console.log(email, password, name);
@@ -56,47 +48,37 @@ const register = async (req, res) => {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    let username = create_username(name, email);
-
-    await client.query('BEGIN'); // Start transaction
-
-    const userExist = await client.query('SELECT * FROM user_login WHERE email = $1', [email]);
+    const userExist = await client.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
 
     if (userExist.rows.length === 0) {
       const hashedPassword = await bcrypt.hash(password, 10);
-      console.log(hashedPassword);
 
       const result = await client.query(
-        'INSERT INTO user_login (email, passwords, user_name) VALUES ($1, $2, $3) RETURNING *',
-        [email, hashedPassword, username]
+        "INSERT INTO users (email, password,name) VALUES ($1, $2, $3) RETURNING *",
+        [email, hashedPassword, name]
       );
 
       const user = result.rows[0];
       const { user_id } = user;
 
-      await client.query(
-        'INSERT INTO user_info (user_id, firstname) VALUES ($1, $2)',
-        [user_id, name]
-      );
-
-      await client.query('COMMIT'); // Commit transaction
-
       const token = jwt.sign({ userId: user_id }, process.env.JWT_SECRET, {
         expiresIn: "1h",
       });
 
-      res.status(201).json({ message: "User registration successful", token: token });
+      res
+        .status(201)
+        .json({ message: "User registration successful", token: token });
     } else {
-      await client.query('ROLLBACK'); // Rollback transaction if user exists
       return res.status(400).json({ message: "User already exists" });
     }
   } catch (error) {
-    await client.query('ROLLBACK'); 
+   
     console.error(error);
     res.status(500).json({ error: "Registration failed" });
-  } 
+  }
 };
-
-
 
 export { login, register };
