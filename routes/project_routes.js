ec9a5a91bcf56f97ee0c8f6a4305859876e50cce
niteshkumar257 <niteshreddy257@gmail.com>
2024-07-19 21:30:11 +0530

    update project_routes

--- routes/project_routes.js
+++ routes/project_routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import {createProject,getAllProjects,getProjectById} from "../controller/project_controller.js"
+import { validateCreateProject,validateGetProjectById } from "../utils/middleware/validator.js";
 const router=Router();
 
 
 
-router.post('/projects', createProject);
+router.post('/project/add',validateCreateProject, createProject);
 router.get('/projects', getAllProjects);
-router.get('/projects/:project_id', getProjectById);
+router.get('/project/:project_id',validateGetProjectById, getProjectById);
 
 export default router;
