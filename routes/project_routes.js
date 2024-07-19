db145c62819202c51b5d4ac7026b3a7430b6e306
niteshkumar257 <niteshreddy257@gmail.com>
2024-07-19 22:51:09 +0530

    add validation middlware

--- routes/project_routes.js
+++ routes/project_routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import {createProject,getAllProjects,getProjectById} from "../controller/project_controller.js"
 import { validateCreateProject,validateGetProjectById } from "../utils/middleware/validator.js";
+import verifyToken from "../utils/middleware/verify_token.js";
 const router=Router();
 
 
 
-router.post('/project/add',validateCreateProject, createProject);
-router.get('/projects', getAllProjects);
-router.get('/project/:project_id',validateGetProjectById, getProjectById);
+router.post('/project/add',verifyToken,validateCreateProject, createProject);
+router.get('/projects', verifyToken,getAllProjects);
+router.get('/project/:project_id',verifyToken,validateGetProjectById, getProjectById);
 
 export default router;
--- routes/task_routes.js
+++ routes/task_routes.js
@@ -6,13 +6,14 @@ import {
     getAllTasksUnderProject
 } from '../controller/task_controller.js';
 import { validateCreateTask, validateGetTaskById } from '../utils/middleware/validator.js';
+import verifyToken from '../utils/middleware/verify_token.js';
 
 const router = express.Router();
 
 
-router.post('/task/:project_id/add', validateCreateTask, createTask);
-router.get('/project/:project_id/user/:user_id/tasks', getTasksUnderProject);
-router.get('/tasks/:task_id', validateGetTaskById, getTaskById);
-router.get('/projects/:project_id/tasks', getAllTasksUnderProject);
+router.post('/task/:project_id/add', verifyToken,validateCreateTask, createTask);
+router.get('/project/:project_id/user/:user_id/tasks',verifyToken, getTasksUnderProject);
+router.get('/tasks/:task_id',verifyToken, validateGetTaskById, getTaskById);
+router.get('/projects/:project_id/tasks',verifyToken, getAllTasksUnderProject);
 
 export default router;
